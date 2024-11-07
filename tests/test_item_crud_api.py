eea96d86998b975ecb25c351bd1de687645877a3
Rittidate Kit <rittidate.kit@truedigital.com>
2024-11-07 08:31:24 +0700

    Seperated test files

--- tests/test_item_crud_api.py
+++ tests/test_item_crud_api.py
@@ -4,13 +4,6 @@ from httpx import AsyncClient
 
 
 @pytest.mark.asyncio
-async def test_root(client: AsyncClient):
-    response = await client.get("/health")
-    assert response.status_code == 200
-    assert response.json() == {"message": "OK!!"}
-
-
-@pytest.mark.asyncio
 async def test_create_item(client: AsyncClient, item_payload):
     response = await client.post("/items/", content=json.dumps(item_payload),)
     assert response.status_code == 201
